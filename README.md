1731d4a0e9cb2f0ee43693148d608ae20b3d67b6
Unknown <xel2754@gmail.com>
2018-05-04 12:53:28 +1200

    Basic setup for discrete commands

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # ENGR101-AVC
 Commands for the Raspberry Pi
-"raspivid -0" - Camera Command
+"raspivid -o" - Camera Command
 "ifconfig" - IPConfig, shows usefu info
 
 SSH connection:
 "ssh -x pi@169.254.160.240"
-Pass?:raspBerry
+Password:"raspberry"
 
+NOTE: IP CHANGES. ADDRESSES BELOW ARE RENDERED USELESS UNLESS WE FIGURE OUT HOW TO SET THE IP AS STATIC.
 Raspberry Pi's IP address: 169.254.160.240
 IPv6: FE80::7e3:a72c:63c3:bd73/64
---
+++ discreteMotorFunctions.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+
+double speed; //Initializes variable for motor speed
+double time; //Initializes variable for turn-time
+
+int main ()
+{
+    // This sets up the RPi hardware and ensures
+    // everything is working correctly
+    init ();
+    // Writes PWM with a duty cycle of 0% to motor 1
+    set_motor (1 ,0);
+
+    // Waits for 0.5 seconds (500000 microseconds )
+    sleep1 (0 ,500000);
+
+    // Writes PWM with a duty cycle of 100% to motor 1
+    set_motor (1 ,255);
+    sleep1 (0 ,500000);
+
+    // Writes PWM with a duty cycle of 0% to motor 1
+    set_motor (1 ,0);
+    sleep1 (0 ,500000);
+
+    return 0;
+}
+
+//Writes a PMW with a duty cycle of 0% to both motors, stopping both.
+void stopMotors();{
+  set_motor(1, 0);
+  set_motor(2, 0);
+}
+
+//Moves both motors in the "forward" direction with a given speed as a parameter
+void forwardMotors(speed){
+  set_motor(1, speed);
+  set_motor(2, speed);
+                          }
+
+//Moves both motors in the "backward" direction with a given speed as a parameter
+void backMotors(speed);{
+ forwardMotors(-speed);
+}
+
+//Turns both motors in opposite directions for [time] milliseconds, with a speed of [speed]
+void spotTurnLeft(speed, time){
+  set_motor(1, speed);
+  set_motor(2, -speed);
+  sleep1 (0, time);
+  stopMotors();
+                                }
+void spotTurnRight(speed, time){
+  spotTurnLeft(-speed, time);
+                                }
