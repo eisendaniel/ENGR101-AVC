63b5b423a2669daeef2da5d4fd3c519db741e7f6
eisendaniel <danieleisen99@gmail.com>
2018-05-04 16:27:22 +1200

    rewrite

--- motortest.cpp
+++ motortest.cpp
@@ -8,19 +8,18 @@ int main ()
     // This sets up the RPi hardware and ensures
     // everything is working correctly
     init ();
-    // Writes PWM with a duty cycle of 0% to motor 1
-    set_motor (1 ,0);
 
-    // Waits for 0.5 seconds (500000 microseconds )
-    sleep1 (0 ,500000);
+    for (int i = 0; i < 4; i++)
+    {
+        set_motor(1, 255);
+        set_motor(2, 255);
 
-    // Writes PWM with a duty cycle of 100% to motor 1
-    set_motor (1 ,255);
-    sleep1 (0 ,500000);
+        sleep1 (0 ,500000);;
 
-    // Writes PWM with a duty cycle of 0% to motor 1
-    set_motor (1 ,0);
-    sleep1 (0 ,500000);
-    
+        set_motor(1, 0);
+        set_motor(2, 0);
+
+        sleep1 (0 ,500000);;
+    }
     return 0;
 }
