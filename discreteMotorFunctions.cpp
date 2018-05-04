b956265ce2b2477b4cfb68318d9737c54e4e2d1b
eisendaniel <danieleisen99@gmail.com>
2018-05-04 16:29:51 +1200

    Format cleanup

--- discreteMotorFunctions.cpp
+++ discreteMotorFunctions.cpp
@@ -25,31 +25,34 @@ int main ()
 }
 
 //Writes a PMW with a duty cycle of 0% to both motors, stopping both.
-void stopMotors(){
+void stopMotors()
+{
   set_motor(1, 0);
   set_motor(2, 0);
 }
 
 //Moves both motors in the "forward" direction with a given speed as a parameter
-void forwardMotors(double speed){
+void forwardMotors(double speed)
+{
   set_motor(1, speed);
   set_motor(2, speed);
-                          }
 
+}
 //Moves both motors in the "backward" direction with a given speed as a parameter
 void backMotors(double speed){
  forwardMotors(-speed);
 }
 
 //Turns both motors in opposite directions for [time] milliseconds, with a speed of [speed]
-void spotTurnLeft(double speed,double time){
+void spotTurnLeft(double speed, double time)
+{
   set_motor(1, speed);
   set_motor(2, -speed);
   sleep1 (0, time);
   stopMotors();
-                                }
+}
 
-                                
-void spotTurnRight(double speed,double time){
+void spotTurnRight(double speed, double time)
+{
   spotTurnLeft(-speed, time);
-                                }
+}
