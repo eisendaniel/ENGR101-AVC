3d7c65c4451131ac34db793c01000fa34d944a0d
eisendaniel <danieleisen99@gmail.com>
2018-05-08 17:27:41 +1200

    fix

--- motortest.cpp
+++ motortest.cpp
@@ -14,12 +14,12 @@ int main ()
         set_motor(1, 255);
         set_motor(2, 255);
 
-        sleep1 (0 ,500000);;
+        sleep1 (0 ,500000);
 
         set_motor(1, 0);
         set_motor(2, 0);
 
-        sleep1 (0 ,500000);;
+        sleep1 (0 ,500000);
     }
     return 0;
 }
