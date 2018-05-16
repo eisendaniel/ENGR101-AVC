a4fee78d5748651147eb0820396fb03829d6083b
eisendaniel <danieleisen99@gmail.com>
2018-05-16 16:01:37 +1200

    neg

--- motortest.cpp
+++ motortest.cpp
@@ -16,6 +16,11 @@ int main ()
 
         sleep1 (0 ,500000);
 
+        set_motor(1, -255);
+        set_motor(2, -255);
+
+        sleep1 (0 ,500000);
+
         set_motor(1, 0);
         set_motor(2, 0);
 
