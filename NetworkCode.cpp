d1b7deee2dd2fa599859005b88e3d97a02e232cf
derpetti <38964608+derpetti@users.noreply.github.com>
2018-05-22 14:46:03 +1200

    Update NetworkCode.cpp

--- NetworkCode.cpp
+++ NetworkCode.cpp
@@ -2,30 +2,25 @@
 #include <time.h>
 #include "E101.h"
 
-int NetworkConnect () {	
+int main () {	
 	connect_to_server("130.195.6.196", 1024);
 	//connect to server of IP and port
+	
+	send_to_server("Please");
+	//sends message to the server
 	
 	char Message[24]; 
 	//create char array
 	
-	Message = ("P", "l", "e", "a", "s", "e"); 
-	//should contain connection password
-	
-	send_to_server(Message);
-	//sends message to the server
-	
 	receive_from_server(Message);
 	//receives message from server, sets to array 'Message'
+	
+	printf("%s", Message);
+	//print server output
 	
 	send_to_server(Message);
 	//sends received message back to server
 	
 	return 0;
 	//end
-}
-int main () {
-	init();
-	NetworkConnect();
-	return 0;
 }
