3e87ff31fe880b56fe1a23abc70ad2a935d96de1
eisendaniel <danieleisen99@gmail.com>
2018-05-25 10:45:12 +1200

    It now compiles :)

--- AdvancedMotorFunctions.cpp
+++ AdvancedMotorFunctions.cpp
@@ -11,7 +11,8 @@ int baseSpeed = 50;
 int minWhiteForQ3 = 10005000;
 
 //=======================General Functions=======================
-void setSpeed(int left, int right) {
+void setSpeed(int left, int right)
+{
 	if (left > 254) left = 254;
 	if (left < -254) left = -254;
 	if (right > 254) right = 254;
@@ -19,32 +20,33 @@ void setSpeed(int left, int right) {
 	right *= -1;
 	set_motor(1, left);
 	set_motor(2, right);
-	
 }
 
 
 //Updates white threshold to account for variations in lighting
-void updateWhiteThreshold() {
+void updateWhiteThreshold()
+{
 	take_picture();
 	int totalWhite = 0;
 	//Loop through whole image
-	for (int r = 0; r < 240; r++) {
-		for (int c = 0; c < 320; c++) {
-			totalWhite += get_pixel(r, c, 3);
-		}
+	for (int r = 0; r < 240; r++)
+	{
+		for (int c = 0; c < 320; c++)
+		{totalWhite += get_pixel(r, c, 3);}
 	}
 	//Set to average of all pixels
 	lineWhiteThreshold = totalWhite / 76800;
 }
 
 //rowStart and rowEnd are inclusive. Remember to take picture before using.
-double getLineErrorSignal(int rowStart, int rowEnd) {
+double getLineErrorSignal(int rowStart, int rowEnd)
+{
 	int error = 0; //If negative, go left, if positive, go right
 	int currWhite;
-	for (int r = rowStart; r <= rowEnd; r++) {
-		//Loop through the rows we are looking at
-		for (int c = 0; c < 320; c++) {
-			//Loop through the columns in the row
+	for (int r = rowStart; r <= rowEnd; r++)
+	{	//Loop through the rows we are looking at
+		for (int c = 0; c < 320; c++)
+		{	//Loop through the columns in the row
 			currWhite = get_pixel(r, c, 3);
 			error += currWhite*(c - 160);
 		}
@@ -54,13 +56,15 @@ double getLineErrorSignal(int rowStart, int rowEnd) {
 }
 
 //Returns number of white pixels in image. Call take picture first.
-int numWhiteInImg(int rowStart, int rowEnd) {
+int numWhiteInImg(int rowStart, int rowEnd)
+{
 	int numWhite;
-	for (int r = rowStart; r < rowEnd; r++) {
-		for (int c = 0; c < 320; c++) {
-			//Loop through image
-			if (get_pixel(r, c, 3) > lineWhiteThreshold) {
-				//Pixel is white
+	for (int r = rowStart; r < rowEnd; r++)
+	{
+		for (int c = 0; c < 320; c++)
+		{	//Loop through image
+			if (get_pixel(r, c, 3) > lineWhiteThreshold)
+			{	//Pixel is white
 				numWhite++;
 			}
 		}
@@ -69,28 +73,33 @@ int numWhiteInImg(int rowStart, int rowEnd) {
 	return numWhite;
 }
 
-bool canSeeLine() {
+bool canSeeLine()
+{
 	int numWhite = numWhiteInImg(220, 230);
-	if (numWhite > minWhiteToSeeLine) {
-		return true;
-	} else return false;
+	if (numWhite > minWhiteToSeeLine)
+	{return true;}
+	else {return false;}
 }
 
-void reverseUntilSeeLine() {
+void reverseUntilSeeLine()
+{
 	setSpeed(-40, -40);
-	while (canSeeLine() == false) {
+	while (canSeeLine() == false)
+	{
 		take_picture();
 		sleep1(0, loopDelay);
 	}
 	setSpeed(0, 0);
 }
 
-void Q3Turn(bool clockwise) {
+void Q3Turn(bool clockwise)
+{
 	int turnSpeed = 100;
 	int speedOne = clockwise ? -turnSpeed : turnSpeed;
 	int speedTwo = clockwise ? turnSpeed : -turnSpeed;
 	setSpeed(speedOne, speedTwo);
-	while (true) {
+	while (true)
+	{
 		take_picture();
 		if (canSeeLine()) return;
 		sleep1(0, loopDelay);
@@ -105,27 +114,31 @@ bool canSeeQ4() {
 //=======================Quadrant Four=======================
 
 void quadFourLoop() {
-	
+
 }
 
 //=======================Quadrant Three=======================
 
-void quadThreeLoop() {
+void quadThreeLoop()
+{
 	double errorSignal;
 	kp = 0.000033;
 	bool clockwise = false;
 	while(true) {
 		take_picture();
-		if (canSeeQ4()) {
-			quadFourLoop();
-		} else if (canSeeLine()) {
-			//Robot is on track	
+		if (canSeeQ4())
+		{quadFourLoop();}
+		else if (canSeeLine())
+		{
+			//Robot is on track
 			printf("Can see line \n");
-			errorSignal = getLineErrorSignal(220, 230);			
+			errorSignal = getLineErrorSignal(220, 230);
 			setSpeed(baseSpeed + errorSignal*kp, baseSpeed - errorSignal*kp);
-		} else {
+		}
+		else
+		{
 			//Robot is not on track
-			printf("Can't see line");	
+			printf("Can't see line");
 			clockwise = !clockwise;
 			Q3Turn(clockwise);
 		}
@@ -135,23 +148,31 @@ void quadThreeLoop() {
 
 //=======================Quadrant Two=======================
 
-void quadTwoLoop() {
+void quadTwoLoop()
+{
 	double errorSignal;
-	while(true) {
-		take_picture();	
-		if (canSeeLine()) {
-			if (numWhiteInImg(220, 230) > minWhiteForQ3) {
+	while(true)
+	{
+		take_picture();
+		if (canSeeLine())
+		{
+			if (numWhiteInImg(220, 230) > minWhiteForQ3)
+			{
 				//Robot has entered Q3
 				break;
-			} else {
-				//Robot is on track	
+			}
+			else
+			{
+				//Robot is on track
 				printf("Can see line \n");
-				errorSignal = getLineErrorSignal(220, 230);			
+				errorSignal = getLineErrorSignal(220, 230);
 				setSpeed(baseSpeed + errorSignal*kp, baseSpeed - errorSignal*kp);
 			}
-		} else {
+		}
+		else
+		{
 			//Robot is not on track
-			printf("Can't see line");	
+			printf("Can't see line");
 			reverseUntilSeeLine();
 		}
 		sleep1(0, loopDelay);
@@ -161,59 +182,55 @@ void quadTwoLoop() {
 
 //=======================Quadrant One=======================
 
-void quadOne() {
+void quadOne()
+{
 	//Add code for opening gate here
-	int main () {	
 	connect_to_server("130.195.6.196", 1024);
 	//connect to server of IP and port
-	
+
 	send_to_server("Please");
 	//sends message to the server
-	
-	char Message[24]; 
+
+	char Message[24];
 	//create char array
-	
+
 	receive_from_server(Message);
 	//receives message from server, sets to array 'Message'
-	
+
 	printf("%s", Message);
 	//print server output
-	
+
 	send_to_server(Message);
 	//sends received message back to server
-	
-	return 0;
-	//end
-}
-	openGate();
+
 	quadTwoLoop();
 }
 
-int image_analysis() {
+int image_analysis()
+{
   take_picture();
   int sum = 0;
   //float kp = 0; //Change this value
   //int proportional_signal = 0;
   int w, s;
-  for (int i = 0; i < 320; i++) {
+  for (int i = 0; i < 320; i++)
+  {
     w = get_pixel(i, 120, 3);
-    if (w > 127) {
-      s = 1;
-    }
-    else {
-      s = 0;
-    }
+    if (w > 127)
+	{s = 1;}
+    else
+	{s = 0;}
     sum = sum + (i - 160) * s;
   }
   //proportional_signal = sum * kp;
   return 0;
 }
 
-int get_ir(int pin) {
+int get_ir(int pin)
+{
   int ir = 0;
-  for (int i = 0; i < 5; i++) {
-    ir = ir + read_analog(pin); //Change this
-  }
+  for (int i = 0; i < 5; i++)
+  {ir = ir + read_analog(pin); /*Change this*/}
   return(ir / 5);
 }
 
@@ -227,9 +244,9 @@ int get_ir(int pin) {
   }
 }*/
 
-int main() 
+int main()
 {
-  init();	
+  init();
   sleep1(15, 0);
   quadOne();
 }
